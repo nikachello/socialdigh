c23cebd1c0cd8e44a3848fe12b8e0836d1104093
nikachello <nikain35@gmail.com>
2024-08-31 00:33:34 +0400

    finished sidebar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        white: "#efeff3",
+        border: "rgba(255, 255, 0.12)",
+      },
     },
   },
   plugins: [],
